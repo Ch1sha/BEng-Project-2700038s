f274ea1415e2f93d445e29a1bb588616e5947f84
Ch1sha <111579379+Ch1sha@users.noreply.github.com>
2025-02-18 21:44:42 +0000

    Refactor Verilog parameter definitions to remove 'int' type; add main function for ADC readout module generation

--- verilog_modules/adc_readout/generate/generate_modules_adc_readout.py
+++ verilog_modules/adc_readout/generate/generate_modules_adc_readout.py
@@ -23,7 +23,7 @@ def update_verilog_parameters(parameters: dict, filename):
         content = file.read()
 
     # Regex pattern to match Verilog parameters (e.g., parameter NAME = VALUE)
-    pattern = r"parameter\s+int\s+(\w+)\s*=\s*(\d+)"
+    pattern = r"parameter\s+(\w+)\s*=\s*(\d+)"
 
     # Function to replace the parameter value
     def replacer(match):
@@ -31,7 +31,7 @@ def update_verilog_parameters(parameters: dict, filename):
         # Replace the parameter value if it's in the dictionary
         if param_name in parameters:
             new_value = parameters[param_name]
-            return f"parameter int {param_name} = {new_value}"
+            return f"parameter {param_name} = {new_value}"
         return match.group(0)  # Keep the original line if parameter not in dictionary
     
     # replace the parameters in the verilog file
@@ -61,5 +61,25 @@ def update_adc_buffer(DATA_WIDTH: int, BUFFER_SIZE: int):
         BUFFER_SIZE (int): Size of the buffer.
     """
 
-    parameters = {"DATA_WIDTH": DATA_WIDTH, "BUFFER_SIZE": BUFFER_SIZE, "ADDR_WIDTH": math.ceil(math.log2(BUFFER_SIZE))}
+    parameters = {"DATA_WIDTH": DATA_WIDTH, "BUFFER_SIZE": BUFFER_SIZE, "ADDR_WIDTH": math.ceil(math.log2(BUFFER_SIZE+1))}
     update_verilog_parameters(parameters, 'adc_buffer.v')
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate Verilog modules for ADC readout')
+    parser.add_argument('--data_width', type=int, default=12, help='Data width of the ADC readout module')
+    parser.add_argument('--buffer_size', type=int, default=4096, help='Size of the buffer')
+    parser.add_argument('--no_generate', action='store_true', help='Do not generate and update the sine wave verilog modules.')
+    args = parser.parse_args()
+
+    if not args.no_generate:
+        print("Generating ADC readout modules...")
+        data_width = args.data_width
+        buffer_size = args.buffer_size
+        print(f"Data width: {data_width}")
+        print(f"Buffer size: {buffer_size}")   
+        update_adc_readout(data_width)
+        update_adc_buffer(data_width, buffer_size)
+
+
+if __name__ == "__main__":
+    main()
